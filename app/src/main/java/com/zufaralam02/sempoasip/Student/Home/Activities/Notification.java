ab4fc4b4eac264e4271061b225d9ba09f45ac3a6
sam <muhammadusamah76@gmail.com>
2018-02-02 11:31:00 +0700

    commit osa

---
+++ app/src/main/java/com/zufaralam02/sempoasip/Student/Home/Activities/Notification.java
@@ -0,0 +1,18 @@
+package com.zufaralam02.sempoasip.Student.Home.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zufaralam02.sempoasip.R;
+
+public class Notification extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+
+
+    }
+}
--- app/src/main/java/com/zufaralam02/sempoasip/Student/Home/Fragments/FragmentHome.java
+++ app/src/main/java/com/zufaralam02/sempoasip/Student/Home/Fragments/FragmentHome.java
@@ -1,19 +1,38 @@
 package com.zufaralam02.sempoasip.Student.Home.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.zufaralam02.sempoasip.R;
+import com.zufaralam02.sempoasip.Student.Home.Activities.Notification;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentHome extends Fragment {
 
+
+    @BindView(R.id.notificationHomeStudent)
+    ImageView notificationHomeStudent;
+    @BindView(R.id.ImageProfileStudent)
+    ImageView ImageProfileStudent;
+    @BindView(R.id.cardAttendance)
+    CardView cardAttendance;
+    @BindView(R.id.cardStudentWallet)
+    CardView cardStudentWallet;
+    Unbinder unbinder;
 
     public FragmentHome() {
         // Required empty public constructor
@@ -23,7 +42,30 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        unbinder = ButterKnife.bind(this, v);
+        return v;
+
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R.id.notificationHomeStudent, R.id.ImageProfileStudent, R.id.cardAttendance, R.id.cardStudentWallet})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.notificationHomeStudent:
+                startActivity(new Intent(getActivity(), Notification.class));
+            case R.id.ImageProfileStudent:
+                break;
+            case R.id.cardAttendance:
+                break;
+            case R.id.cardStudentWallet:
+                break;
+        }
+    }
 }
