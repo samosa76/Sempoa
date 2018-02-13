b6184a03865c9cd34301acd47915d34876e80ba6
sam <muhammadusamah76@gmail.com>
2018-02-13 13:08:44 +0700

    penyelesaian mockup

--- app/src/main/java/com/zufaralam02/sempoasip/Student/Home/Activities/DetailWithDraw.java
+++ app/src/main/java/com/zufaralam02/sempoasip/Student/Home/Activities/DetailWithDraw.java
@@ -1,15 +1,44 @@
 package com.zufaralam02.sempoasip.Student.Home.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.iapps.adapters.BaseRecyclerAdapter;
+import com.zufaralam02.sempoasip.Base.BaseActivitySempoa;
 import com.zufaralam02.sempoasip.R;
 
-public class DetailWithDraw extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class DetailWithDraw extends BaseActivitySempoa {
+
+    @BindView(R.id.textWithDraw)
+    TextView textWithDraw;
+    @BindView(R.id.btnFinishWithDraw)
+    Button btnFinishWithDraw;
+
+    String dataWithDraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_with_draw);
+        ButterKnife.bind(this);
+
+        dataWithDraw = getIntent().getStringExtra("data");
+
+        textWithDraw.setText("Withdraw RP" + dataWithDraw + " Success!");
+
+        setupNav("DetailRequestCoin");
+    }
+
+    @OnClick(R.id.btnFinishWithDraw)
+    public void onClick() {
+
+        finish();
+
     }
 }
